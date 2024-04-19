7ab9ec0bb771c72413a9b853e6438f5b83c20fde
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-04-19 11:50:40 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main() {
+	long long a, b;
+	cin >> a>>b;
+	if (a == b)
+		cout << 1;
+	else
+		cout << 0;
+	return 0;
+}
