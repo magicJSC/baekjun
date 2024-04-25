50c0f0804a7eba1e3c87b086ed39dd0900a4f32d
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-04-25 15:30:22 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+int main() {
+	string a;
+	cin >> a;
+	cout << ":fan::fan::fan:\n:fan::" << a << "::fan:\n:fan::fan::fan:";
+	return 0;
+}
