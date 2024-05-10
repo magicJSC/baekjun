4ee64eb58b94a5e8798cdeeb02bdc71d020cab56
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-10 09:13:43 +0900

    [Bronze V] Title: Shares, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main() {
+	long long a, b;
+	while (cin >> a >> b)
+	{
+		cout << b / (a + 1) << "\n";
+	}
+	return 0;
+}
