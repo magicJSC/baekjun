927cad921fcfdc1581e195e215135c38131456c4
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-15 08:56:12 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << b-a << " " << b;
+	return 0;
+}
