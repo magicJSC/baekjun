261f73c5b778c1eda0912e90b143bb342bf873ba
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-17 17:27:37 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main() {
+	int a, b,c,d;
+	cin >> a >> b >> c >> d;
+	cout << a*b+c*d;
+	return 0;
+}
