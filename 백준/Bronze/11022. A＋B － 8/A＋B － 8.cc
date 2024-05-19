e3066639e02ac146a29ecc244514cfb6751efe89
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-20 08:47:17 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a;
+	for (int i = 1; i <= a; i++) {
+		cin >> b >> c;
+		cout << "Case #" << i << ": " <<b<<" + " << c <<" = "<< b + c << "\n";
+	}
+	return 0;
+}
