0b0a49e6bd033c07b321005d1a5187394226dbc1
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-05-25 18:41:33 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	cout << a-543;
+	return 0;
+}
