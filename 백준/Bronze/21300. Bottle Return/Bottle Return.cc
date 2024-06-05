cbc7b5c85715dff2ca4c2bc1c71d0746638cf652
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-06-05 09:23:38 +0900

    [Bronze V] Title: Bottle Return, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a1,a2,a3,a4,a5,a6;
+	cin >> a1>>a2>>a3>>a4>>a5>>a6;
+	
+	cout << a1 * 5 + a2 * 5+a3*5+a4*5+a5*5+a6*5;
+	return 0;
+}
