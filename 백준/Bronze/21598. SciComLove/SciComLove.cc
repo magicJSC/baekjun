3d8a5fc9bbdbeb7286cc4a56f9eefb7e21594893
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-06-09 10:29:13 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a;
+	cin >> a;
+	if (a == 1)
+		cout << "SciComLove";
+	else if (a == 2)
+		cout << "SciComLove\n" << "SciComLove";
+	return 0;
+}
