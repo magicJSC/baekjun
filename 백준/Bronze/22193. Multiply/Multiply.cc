40a9885273b57399b46e5ae419932fb8d37b105c
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-06-10 08:48:15 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a,b;
+	cin >> a >> b>>a>>b;
+	cout << a * b;
+	return 0;
+}
