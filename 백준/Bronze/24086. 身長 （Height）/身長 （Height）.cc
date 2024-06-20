2c7e2498c4fbf67bb93f691a302f32f1735150a6
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-06-21 08:40:32 +0900

    [Bronze V] Title: 身長 (Height), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+	int a,b;
+	cin >> a>>b;
+	cout << b-a;
+	return 0;
+}
