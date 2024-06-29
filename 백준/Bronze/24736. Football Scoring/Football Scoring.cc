d3237a996888ef337edf705f64b8d534c6f4a26d
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-06-29 15:57:43 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	cout << a * 6 + b * 3 + c * 2 + d + e * 2<<" ";
+	cin >> a >> b >> c >> d >> e;
+	cout << a * 6 + b * 3 + c * 2 + d + e * 2;
+
+	return 0;
+}
