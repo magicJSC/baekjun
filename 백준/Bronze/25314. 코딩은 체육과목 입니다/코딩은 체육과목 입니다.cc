b76c5cf31fd16112770a11a4adad89ed4b7494bf
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-01 11:20:58 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 0; i < a / 4; i++) {
+		cout << "long ";
+	}
+	cout << "int";
+	return 0;
+}
