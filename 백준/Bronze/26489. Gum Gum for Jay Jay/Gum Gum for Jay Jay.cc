19ead419e8b3dd99beead7e47b97f654f91c550e
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-07 12:16:19 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+	string s;
+	int count = 0;
+	while (getline(cin, s)) {
+		
+		count++;
+	}
+	cout << count;
+	return 0;
+}
