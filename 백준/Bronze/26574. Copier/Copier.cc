6bd0f5106dbde1e7b1c5168eb5d70e955672d9de
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-09 12:27:32 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+	int a,b;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cin >> b;
+		cout << b << " " << b << "\n";
+	}
+
+	return 0;
+}
