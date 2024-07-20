ae70aadfc3635966d039d1920f2021061a81247a
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-20 09:16:51 +0900

    [Bronze V] Title: 時間 (Hour), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+	int a;
+	cin >> a;
+	cout << a * 24;
+	return 0;
+}
