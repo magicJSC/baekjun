cabca3cbcf82d1db5e7935bfb202054c30526ccc
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-22 08:49:28 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * 10 + b;
+
+	return 0;
+}
