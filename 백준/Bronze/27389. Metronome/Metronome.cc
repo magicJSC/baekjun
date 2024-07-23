74aee38fa134cec22bea1d7245e5802d664e7147
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-23 10:16:25 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main() {
+
+	
+
+	float a;
+	cin >> a;
+	cout << fixed;
+	cout.precision(2);
+	cout << a / 4;
+	return 0;
+}
