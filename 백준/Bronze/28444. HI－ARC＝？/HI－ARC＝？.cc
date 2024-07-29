b0f02e5574e34332678c528f718b149269d57186
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-07-29 14:14:57 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a,b,c,d,e;
+	cin >> a >> b>> c>> d>> e;
+
+	cout << a * b - c * d * e;
+
+	return 0;
+}
+
+
