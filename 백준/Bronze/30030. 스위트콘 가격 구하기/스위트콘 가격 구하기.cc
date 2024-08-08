97dd65fbcc44f86e6d41347ecf6be07df3b75feb
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-08 09:18:27 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+
+	int a;
+	cin >> a;
+	cout << a * 10 / 11;
+	return 0;
+}
+
+
