432bd4521e5ec947264575b175d1bb9050b84af9
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-14 08:56:00 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a;
+	cin >> a;
+	if (a == 1)
+		cout << "12 1600";
+	else if(a == 2)
+		cout << "11 894";
+	else if (a == 3)
+		cout << "11 1327";
+	else if (a == 4)
+		cout << "10 1311";
+	else if (a == 5)
+		cout << "9 1004";
+	else if (a == 6)
+		cout << "9 1178";
+	else if (a == 7)
+		cout << "9 1357";
+	else if (a == 8)
+		cout << "8 837";
+	else if (a == 9)
+		cout << "7 1055";
+	else if (a == 10)
+		cout << "6 556";
+	else if (a == 11)
+		cout << "6 773";
+	
+	return 0;
+}
+
+
