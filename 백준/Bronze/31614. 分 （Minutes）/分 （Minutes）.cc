0023edeeff989925c26cc755afd79c70da559b90
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-19 08:46:09 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a * 60 + b;
+	
+	return 0;
+}
+
+
