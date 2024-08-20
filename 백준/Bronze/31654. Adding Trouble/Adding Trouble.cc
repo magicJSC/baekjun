cc48def4c76872d17173d5c6ffbbc95f9f55f9c6
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-21 08:44:23 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a + b == c)
+		cout << "correct!";
+	else
+		cout << "wrong!";
+	return 0;
+}
+
+
