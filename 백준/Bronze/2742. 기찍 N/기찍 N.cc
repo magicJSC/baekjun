c3857125129de8514b917ce571e95fee2b9d6330
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-22 09:15:02 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = a; i > 0; i--)
+	{
+		cout << i << "\n";
+	}
+	return 0;
+}
+
+
