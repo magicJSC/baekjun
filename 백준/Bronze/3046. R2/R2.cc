d287afff1ccb77c5121fb31e642ce4a49fced936
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-08-31 12:00:52 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	fastIo;
+	int a, b;
+	cin >> a >> b;
+	cout << b * 2 - a;
+	return 0;
+}
+
+
