87403e779bde391cf49b4a8c453aa6686d9750b3
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-09-13 14:10:59 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	while (true) {
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+			break;
+		cout << a + b << "\n";
+	}
+	
+
+	return 0;
+}
+
+
+
+
