a121f16c2dc23788a40bb8b303ee1283426c5e32
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-09-16 20:40:57 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a,b,c;
+	cin >> a >> b >>c;
+	if (a <= b / c)
+		cout << 1;
+	else
+		cout << 0;
+
+	return 0;
+}
+
+
+
+
