b0a9e6704ae55597c4af763befda32ca98f84b62
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-09-28 18:55:09 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	double a,b;
+	cout.precision(8);
+	cin >> a >> b;
+	cout << a * 2 + 2 * b * 3.141592;
+	return 0;
+}
+
+
+
+
