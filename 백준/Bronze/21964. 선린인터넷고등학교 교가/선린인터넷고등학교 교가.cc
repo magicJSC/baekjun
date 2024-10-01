a51e7b4a1cc7784f32fa89d335c8da57874dc74f
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-10-02 08:40:54 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		char c;
+		cin >> c;
+		if (i >= a - 5)
+			cout << c;
+	}
+	return  0;
+}
+
+
+
+
