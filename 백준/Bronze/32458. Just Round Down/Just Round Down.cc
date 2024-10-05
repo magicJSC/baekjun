2bbaa84507172a6d86226e1a01bbbeacbe33d875
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-10-05 20:14:57 +0900

    [Bronze V] Title: Just Round Down, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32458. Just Round Down/Just Round Down.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	float a;
+	cin >> a;
+	cout << (int)a;
+	return  0;
+}
