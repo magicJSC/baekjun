013768ed26a7aa880e495fef4465e3c96b152532
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-10-25 08:26:22 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	fastIo;
+	int a;
+	cin >> a;
+	cout << a << "\n";
+	
+		cout << 1;
+}
