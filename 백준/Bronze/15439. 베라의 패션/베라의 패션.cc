b43fee1ac758dc59b5f013e1b0c413c0aab2a0b6
정성찬 <133185243+magicJSC@users.noreply.github.com>
2024-10-28 08:41:37 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <deque>
+using namespace std;
+
+#define fastIo ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+	fastIo;
+	int a;
+	cin >> a;
+	cout << a * a - a;
+}
